f2d9f8284d7ad89ff8b98a4b43402d30c1ad558d
irshad-ai <irshadsheikh2005@gmail.com>
2024-05-09 18:34:48 +0530

    "Simmer ui"

--- Magic_Food/src/components/Body.js
+++ Magic_Food/src/components/Body.js
@@ -2,39 +2,7 @@ import { useState, useEffect } from "react";
 import RestaurantCard from "./RestaurantCard";
 const Body = () => {
   const [listOfRestaurant, setListOfRestaurant] = useState([
-    {
-      info: {
-        id: "520021",
-        name: "Pizza Hut",
-        cloudinaryImageId: "2b4f62d606d1b2bfba9ba9e5386fabb7",
-        costForTwo: "₹350 for two",
-        cuisines: ["Pizzas"],
-        avgRating: 3.8,
-        deliveryTime: 74,
-      },
-    },
-    {
-      info: {
-        id: "520022",
-        name: "UBQ by Barbeque Nation",
-        cloudinaryImageId: "wtxzaleh5he39fudgshh",
-        costForTwo: "₹300 for two",
-        cuisines: ["North Indian", "Barbecue", "Biryani"],
-        avgRating: 4.1,
-        deliveryTime: 52,
-      },
-    },
-    {
-      info: {
-        id: "520023",
-        name: "McDonald's",
-        cloudinaryImageId: "535fc9f9c135f7982317bbb6a64a1ffc",
-        costForTwo: "₹300 for two",
-        cuisines: ["North Indian", "Barbecue", "Biryani"],
-        avgRating: 4.3,
-        deliveryTime: 47,
-      },
-    },
+    
   ]);
 
   const getData = async () => {
@@ -50,7 +18,12 @@ const Body = () => {
   useEffect(() => {
     getData();
   }, []);
-
+{
+  if(listOfRestaurant ==0)
+    {
+      return <p>Loading</p>
+    }
+}
   return (
     <div className="body">
       <div className="top-rated">
